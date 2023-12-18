f269d32a2899b2c63aff45e84b6b96629c8a5c89
babaohuang <babaohuang@gmail.com>
2023-12-18 16:56:21 +0800

    Revert "Update openAI.ts"
    This reverts commit 204699114f5848e02d5c057a1a2dbb6a1a4e9b91.

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -23,53 +23,17 @@ export const startChatAndSendMessageStream = async(history: ChatMessage[], newMe
   // Use sendMessageStream for streaming responses
   const result = await chat.sendMessageStream(newMessage)
 
-  const bufferedStream = new ReadableStream({
+  const encodedStream = new ReadableStream({
     async start(controller) {
       const encoder = new TextEncoder()
-      const decoder = new TextDecoder('utf-8', { stream: true })
-      let buffer = ''
-
       for await (const chunk of result.stream) {
-        // Combine the buffer with the new chunk
-        const combinedChunk = buffer + decoder.decode(chunk, { stream: true })
-
-        // Find the last complete character
-        const lastCompleteCharIndex = findLastCompleteCharIndex(combinedChunk)
-
-        // The complete text is everything up to the last complete character
-        const completeText = combinedChunk.slice(0, lastCompleteCharIndex)
-
-        // Buffer the rest (incomplete character or empty if none)
-        buffer = combinedChunk.slice(lastCompleteCharIndex)
-
-        // Enqueue the complete text to the stream
-        controller.enqueue(encoder.encode(completeText))
+        const text = await chunk.text()
+        const encoded = encoder.encode(text) // 对文本进行UTF-8编码
+        controller.enqueue(encoded)
       }
-
-      // Decode and enqueue any remaining buffered text
-      if (buffer.length > 0)
-        controller.enqueue(encoder.encode(decoder.decode(buffer)))
-
       controller.close()
     },
   })
 
-  return bufferedStream
-}
-
-// Helper function to find the index of the last complete UTF-8 character
-function findLastCompleteCharIndex(text) {
-  const encoder = new TextEncoder()
-  for (let i = text.length; i >= 0; i--) {
-    // Try to encode the text up to the current index
-    try {
-      // If this does not throw, it means we have a valid UTF-8 string
-      encoder.encode(text.slice(0, i))
-      return i // Return the index of the last valid character
-    } catch (e) {
-      // If an error is thrown, it means the character at the current index is not complete
-      // We continue the loop to try with one less character
-    }
-  }
-  return 0 // If no valid character was found, return 0
+  return encodedStream
 }
